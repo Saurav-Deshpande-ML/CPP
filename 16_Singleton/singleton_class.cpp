2eb73a8698dbe5e425ee2b68ba0bd2bcabe455d3
Saurav-Deshpande <111336016+Saurav-Deshpande@users.noreply.github.com>
2022-09-08 11:08:24 +0530

    Update singleton_class.cpp

--- 16_Singleton/singleton_class.cpp
+++ 16_Singleton/singleton_class.cpp
@@ -1,32 +1,36 @@
-#include<iostream>
-#include<string>
-#include"singleton.hpp"
+#include <iostream>
+#include <string>
+#include "singleton.hpp"
 
 using namespace std;
 
-int Singleton :: counter = 0;
-Singleton* Singleton::instance =nullptr;
+int Singleton ::counter = 0;
+Singleton *Singleton::instance = nullptr;
 mutex Singleton ::mtx;
 
-Singleton :: Singleton(){
+Singleton ::Singleton()
+{
 
     counter++;
-    cout<<"Number of Instances: "<<counter<<endl;
-
+    cout << "Number of Instances: " << counter << endl;
 }
 
-Singleton* Singleton :: getSingleInstance(){
+Singleton *Singleton ::getSingleInstance()
+{
 
-    if(instance==nullptr){
+    if (instance == nullptr)
+    {
         mtx.lock();
-        if(instance==nullptr){
-            instance=new Singleton();
+        if (instance == nullptr)
+        {
+            instance = new Singleton();
         }
         mtx.unlock();
     }
     return instance;
 }
 
-void Singleton :: setMessage(string msg){
-    cout<<msg<<endl;
+void Singleton ::setMessage(string msg)
+{
+    cout << msg << endl;
 }
