743f4add3dce1d1b8d040a7720b6eca919fb1f7e
Saurav-Deshpande <111336016+Saurav-Deshpande@users.noreply.github.com>
2022-09-09 18:00:21 +0530

    Create main.cpp

---
+++ makefile/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"header.h"
+
+using namespace std;
+
+int main(){
+
+    cout<<"Main start"<<endl;
+    cout<<"Greetings: ";
+    greetings();
+    cout<<"Add: ";
+    add(7,9);
+    cout<<"Main end"<<endl;
+}
